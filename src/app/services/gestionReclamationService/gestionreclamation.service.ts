db81c637882cbab431c2b0fd159b90509cff4a76
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-07-25 21:40:49 +0100

    ajouter reclamation DONE

---
+++ src/app/services/gestionReclamationService/gestionreclamation.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserStateService } from '../user-state.service';
+import { Reclamation } from '../../../core/models/Reclamation';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GestionreclamationService {
+
+
+  private baseUrl = 'http://localhost:8089/BackStageTel/reclamation';
+
+  constructor(private http:HttpClient, private router:Router, private userStateService: UserStateService) { }
+
+
+  ajouterReclamation(reclamation: Reclamation, idUser: number): Observable<Reclamation> {
+    return this.http.post<Reclamation>(`${this.baseUrl}/ajouter/${idUser}`, reclamation);
+  }
+}
